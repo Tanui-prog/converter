0c9f9b909838a0c940723f61518723def465d747
Tanui <vikiikiptoo@gmail.com>
2024-07-09 13:35:04 +0300

    refactor: Update background image URL in index.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import ConverterForm from "./components/ConverterForm";
 
 const App = () => {
   return (
-    <div className="currency-converter">
+    <div className="currency-converter" >
       <h2 className="converter-title">Currency Converter</h2>
       <ConverterForm />
     </div>
